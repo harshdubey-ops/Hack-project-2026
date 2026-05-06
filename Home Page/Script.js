3335ccc08244b9a5d9d7c6369a631e651f39c615
Aryan <277413363+aryankhrwar964-sudo@users.noreply.github.com>
2026-05-06 09:57:30 -0700

    change in button

--- Home Page/Script.js
+++ Home Page/Script.js
@@ -1,7 +1,23 @@
-let arr = [1,2,3,4,5,6];
+let x = document.querySelector("#temp1");
+x.style.backgroundColor = "rgb(15, 131, 15)";
+x.style.color = "white";
+x.addEventListener("mouseenter",function(){
+    x.style.backgroundColor = "rgb(89, 194, 29)";
+    x.style.color = "black";
+})
+x.addEventListener("mouseleave",function(){
+    x.style.backgroundColor = "rgb(15, 131, 15)";
+    x.style.color = "white";
+})
 
-let newArr = arr.filter((val) => {
-    return val%2 === 0;
-});
-
-console.log(newArr);
+let y = document.querySelector("#temp2");
+y.style.backgroundColor = "rgb(15, 131, 15)";
+y.style.color = "white";
+y.addEventListener("mouseenter",function(){
+    y.style.backgroundColor = "rgb(89, 194, 29)";
+    y.style.color = "black";
+})
+y.addEventListener("mouseleave",function(){
+    y.style.backgroundColor = "rgb(15, 131, 15)";
+    y.style.color = "white";
+})
